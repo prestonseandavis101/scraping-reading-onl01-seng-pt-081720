7ea873501ce787cb22a70d5ccd9fa2ab1521c494
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-26 15:44:40 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,8 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+nokogiri::HTML(html)
+
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+doc.css(".headline-260IBN")
